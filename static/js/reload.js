4596613e65581712f6c230f87971edfabd0ae919
Daniel Friesel <derf@finalrewind.org>
2021-08-22 11:36:40 +0200

    add 'synchronization failed' marker

--- static/js/reload.js
+++ static/js/reload.js
@@ -1,12 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const departureList = document.getElementById('departurelist');
+	const syncFailedMarker = document.getElementById('syncfailedmarker');
 
 	const showDepartures = (departureText) => {
 		departureList.innerHTML = departureText;
 	};
 
 	const fetchDepartures = () => {
-		fetch(window.location.href + '?ajax=1').then(response => response.text()).then(showDepartures);
+		fetch(window.location.href + '?ajax=1')
+			.then(response => {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.text()
+			})
+			.then(departureText => {
+				syncFailedMarker.style.display = "none";
+				showDepartures(departureText);
+			})
+			.catch(err => {
+				syncFailedMarker.style.display = "inline-block";
+			});
 	};
 
 	setInterval(fetchDepartures, 60000);
